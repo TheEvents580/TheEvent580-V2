0a0d9b03fcb25b76db450301b4bfe43fe8ddf0b4
Fox <44817153+TheFox580@users.noreply.github.com>
2025-11-10 01:08:08 +0100

    Sorted games by game condition... And added a new game :)

--- src/main/java/fr/thefox580/theevent5802/utils/Game.java
+++ src/main/java/fr/thefox580/theevent5802/utils/Game.java
@@ -5,27 +5,28 @@ import java.util.List;
 
 public enum Game {
 
-    TRIALS("Trials", ColorType.MC_RED, "\uE001", true, 10, 10*60),
-    PARKOUR("Parkour", ColorType.MC_ORANGE, "\uE002", true, 2, 10*60),
-    FINDER("Finder", ColorType.MC_YELLOW, "\uE003", true, 13, 15*60),
-    TAG("Tag", ColorType.MC_LIME, "\uE004", true, 11, 12*60),
-    MULTILAP("Multilap", ColorType.MC_AQUA, "\uE005", true, 5, 10*60),
-    BUILD_MASTERS("Build Masters", ColorType.MC_BLUE, "\uE006", true, 9, 60*60),
-    ARMS_RACE("Arms Race", ColorType.MC_PURPLE, "\uE007", true, 12, 15*60),
-    BOW_PVP("Bow PVP", ColorType.MC_PINK, "\uE008", true, 8, 10*60),
     HUB("Hub", ColorType.TEXT, "", false, 0, -1),
     @Deprecated
     DROPPER("Dropper", ColorType.MC_RED, "?", false, 1, 10*60),
+    PARKOUR("Parkour", ColorType.MC_ORANGE, "\uE002", true, 2, 10*60),
     @Deprecated
     BINGO("Bingo", ColorType.MC_YELLOW, "\uE003", false, 3, 20*60),
     @Deprecated
     FIND_THE_BUTTON("Find The Button", ColorType.MC_LIME, "?", false, 4, 12*60+30),
     @Deprecated
     RACES("Races", ColorType.MC_AQUA, "?", false, 5, 10*60),
+    MULTILAP("Multilap", ColorType.MC_AQUA, "\uE005", false, 5, 10*60),
     @Deprecated
     SURVIVAL_GAMES("Survival Games", ColorType.MC_PURPLE, "\uE007", false, 6, -1),
     @Deprecated
-    LABYRINTH("Labyrinth", ColorType.MC_PURPLE, "?", false, 7, 10*60);
+    LABYRINTH("Labyrinth", ColorType.MC_PURPLE, "?", false, 7, 10*60),
+    BOW_PVP("Bow PVP", ColorType.MC_PINK, "\uE008", true, 8, 10*60),
+    BUILD_MASTERS("Build Masters", ColorType.MC_BLUE, "\uE006", true, 9, 60*60),
+    TRIALS("Trials", ColorType.MC_RED, "\uE001", true, 10, 10*60),
+    TAG("Tag", ColorType.MC_LIME, "\uE004", true, 11, 12*60),
+    ARMS_RACE("Arms Race", ColorType.MC_PURPLE, "\uE007", true, 12, 15*60),
+    FINDER("Finder", ColorType.MC_YELLOW, "\uE003", true, 13, 15*60),
+    MULTIRACE("Multi Race", ColorType.MC_AQUA, "?", true, 14, 15*60);
 
     private final String name;
     private final ColorType color;
